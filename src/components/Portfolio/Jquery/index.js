12fb1c38d799f18daa3fc92a394ef372eb4d5642
alperenkursun <alperenkursun@outlook.com>
2023-01-09 21:05:11 +0300

    mini update

--- src/components/Portfolio/Jquery/index.js
+++ src/components/Portfolio/Jquery/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import styles from "./styles.module.css";
-import project from "../project.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faBootstrap,
