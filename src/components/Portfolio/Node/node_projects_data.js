94b82c376fc6dd4cfa90361958dc277f443f2153
alperenkursun <alperenkursun@outlook.com>
2024-09-17 23:42:50 +0300

    The code has been refactored for clarity

---
+++ src/components/Portfolio/Node/node_projects_data.js
@@ -0,0 +1,32 @@
+import cleanblogproject from "../images/cleanblogproject.webp";
+
+const node_projects_data = [
+  {
+    img: cleanblogproject,
+    isResponsive: false,
+    title: "Clean Blog App",
+    html: false,
+    css: false,
+    bootstrap: false,
+    sass: false,
+    js: true,
+    jqueryy: false,
+    jqueryyui: false,
+    tailwindd: false,
+    react: false,
+    reduxx: false,
+    chakra: false,
+    node: true,
+    mongo: true,
+    isCodeBtn: {
+      isThere: true,
+      link: "https://github.com/alperenkursun/clean-blog-project",
+    },
+    isSeeBtn: {
+      isThere: true,
+      link: "https://clean-blog-we99.onrender.com/",
+    },
+  },
+];
+
+export default node_projects_data;
---
+++ src/components/ProjectCard/index.js
@@ -0,0 +1,239 @@
+import styles from "./styles.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBootstrap,
+  faCss3Alt,
+  faHtml5,
+  faNode,
+  faReact,
+  faSass,
+  faSquareJs,
+} from "@fortawesome/free-brands-svg-icons";
+import { faCode } from "@fortawesome/free-solid-svg-icons";
+import { faEye } from "@fortawesome/free-regular-svg-icons";
+import { useDarkMode } from "../../contexts/DarkModeContext";
+import jqueryui from "../Portfolio/images/jqueryui.webp";
+import jquery from "../Skills/jquery.webp";
+import chakraa from "../Skills/chakra.webp";
+import tailwind from "../Portfolio/images/tailwind.webp";
+import redux from "../Portfolio/redux.webp";
+import mongodb from "../Portfolio/images/mongodb.webp";
+
+function ProjectCard({
+  img,
+  isResponsive,
+  title,
+  html,
+  css,
+  bootstrap,
+  sass,
+  js,
+  jqueryy,
+  jqueryyui,
+  tailwindd,
+  react,
+  reduxx,
+  chakra,
+  node,
+  mongo,
+  isCodeBtn,
+  isSeeBtn,
+}) {
+  const { isDarkMode } = useDarkMode();
+  return (
+    <div className={styles.project}>
+      {isResponsive && <div className={styles.responsive}>Responsive</div>}
+      <img src={img} alt="projectimg" className={styles.projectImg} />
+      <div
+        className={`${styles.projectName} ${
+          isDarkMode ? styles.projectNameDark : styles.projectNameLight
+        }`}
+      >
+        {title}
+      </div>
+      <div className={styles.projectTechs}>
+        Technologies:{" "}
+        {html && (
+          <FontAwesomeIcon
+            icon={faHtml5}
+            style={{ color: "rgb(228,77,38)" }}
+            className={styles.tech}
+          />
+        )}
+        {css && (
+          <FontAwesomeIcon
+            icon={faCss3Alt}
+            style={{ color: "rgb(37,75,221)" }}
+            className={styles.tech}
+          />
+        )}
+        {bootstrap && (
+          <FontAwesomeIcon
+            icon={faBootstrap}
+            style={{ color: "rgb(123,17,243)" }}
+            className={styles.tech}
+          />
+        )}
+        {sass && (
+          <FontAwesomeIcon
+            icon={faSass}
+            style={{ color: "rgb(199,100,148)" }}
+            className={styles.tech}
+          />
+        )}
+        {js && (
+          <FontAwesomeIcon
+            icon={faSquareJs}
+            style={{ color: "rgb(226,196,48)" }}
+            className={styles.tech}
+          />
+        )}
+        {jqueryy && (
+          <img
+            src={jquery}
+            alt="jquery"
+            className={styles.tech}
+            style={{
+              borderRadius: "100%",
+            }}
+          />
+        )}
+        {jqueryyui && (
+          <img
+            src={jqueryui}
+            alt="jquery"
+            className={styles.tech}
+            style={{
+              borderRadius: "100%",
+            }}
+          />
+        )}
+        {tailwindd && (
+          <img
+            src={tailwind}
+            alt="tailwind"
+            className={styles.tech}
+            style={{
+              borderRadius: "100%",
+            }}
+          />
+        )}
+        {react && (
+          <FontAwesomeIcon
+            icon={faReact}
+            style={{ color: "rgb(94,211,243)" }}
+            className={styles.tech}
+          />
+        )}
+        {reduxx && (
+          <img
+            src={redux}
+            alt="chajkra"
+            className={styles.tech}
+            style={{
+              borderRadius: "100%",
+            }}
+          />
+        )}
+        {chakra && (
+          <img
+            src={chakraa}
+            alt="chajkra"
+            className={styles.tech}
+            style={{
+              borderRadius: "100%",
+            }}
+          />
+        )}
+        {node && (
+          <FontAwesomeIcon
+            icon={faNode}
+            style={{ color: "rgb(114,169,96)" }}
+            className={styles.tech}
+          />
+        )}
+        {mongo && (
+          <img
+            src={mongodb}
+            alt="jquery"
+            style={{ width: "40px", height: "35px" }}
+            className={styles.tech}
+          />
+        )}
+      </div>
+      <div
+        className={`${styles.projectButtons} ${
+          isDarkMode ? styles.projectButtonsDark : styles.projectButtonsLight
+        }`}
+      >
+        {isCodeBtn.isThere ? (
+          <a
+            className={`${styles.projectButton1} ${
+              isDarkMode
+                ? styles.projectButton1Dark
+                : styles.projectButton1Light
+            }`}
+            href={isCodeBtn.link}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faCode} className={styles.tech} /> Code
+          </a>
+        ) : (
+          <div
+            className={`${styles.projectButton1} ${
+              isDarkMode
+                ? styles.projectButton1Dark
+                : styles.projectButton1Light
+            }`}
+            aria-disabled
+            style={{
+              backgroundColor: "rgb(153, 153, 153)",
+              cursor: "context-menu",
+              opacity: 1,
+            }}
+          >
+            <FontAwesomeIcon icon={faCode} className={styles.tech} /> Code
+          </div>
+        )}
+
+        {isSeeBtn.isThere ? (
+          <a
+            className={`${styles.projectButton2} ${
+              isDarkMode
+                ? styles.projectButton2Dark
+                : styles.projectButton2Light
+            }`}
+            href={isSeeBtn.link}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faEye} className={styles.tech} /> See The
+            Project
+          </a>
+        ) : (
+          <div
+            className={`${styles.projectButton2} ${
+              isDarkMode
+                ? styles.projectButton2Dark
+                : styles.projectButton2Light
+            }`}
+            style={{
+              backgroundColor: "#999999",
+              cursor: "context-menu",
+              opacity: "1",
+            }}
+            // href=""
+            // target="_blank"
+            // rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faEye} className={styles.tech} /> See The
+            Project
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default ProjectCard;
